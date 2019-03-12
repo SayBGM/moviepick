52161f137bbe5b53c402c64445b5117c7e566818
SayBGM <bgm537@saybgm.me>
2019-03-12 19:46:44 +0900

    구조 변경

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-// import { Provider } from 'react-redux';
 import { BrowserRouter, Switch } from 'react-router-dom';
 
 const App = () => (
-  // <Provider>
   <BrowserRouter>
     <Switch>
       <div className="root">Hello! Parcel's React!</div>
     </Switch>
   </BrowserRouter>
-  // </Provider>
 );
 
 export default App;
