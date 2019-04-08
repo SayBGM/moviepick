1a3be81f9cbcbebd5d1b626fa1101663b9643c60
SayBGM <bgm537@saybgm.me>
2019-04-08 21:59:12 +0900

    redux type 추가

---
+++ src/core/redux/actions/types.js
@@ -0,0 +1,89 @@
+// 영화 정보 가져오기
+/**
+ * 영화 정보 가져오기
+ */
+export const GET_MOVIE = 'GET_MOVIE';
+
+/**
+ * 영화 정보 ajax 통신 시작
+ */
+export const LODING_GET_MOVIE = 'LODING_GET_MOVIE'
+
+/**
+ * 영화 정보 ajax 통신 종료
+ */
+export const END_GET_MOVIE = 'END_GET_MOVIE'
+
+/**
+ * 영화 정보 통신 성공
+ */
+export const SUCCESS_GET_MOVIE = 'SUCCESS_GET_MOVIE'
+
+/**
+ * 영화 정보 통신 실패
+ */
+export const FAIL_GET_MOVIE = 'FAIL_GET_MOVIE'
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// 영화 비교
+/**
+ * 비교 장바구니 담기
+ */
+export const SELECT_MOVIE = 'SELECT_MOVIE'
+
+/**
+ * 비교 장바구니에서 영화 빼기
+ */
+export const DELETE_MOVIE = 'DELETE_MOVIE'
+
+/**
+ * 영화 비교 정보 가져오기
+ */
+export const GET_COMPARE_MOVIE_LIST = 'GET_COMPARE_MOVIE_LIST'
+
+/**
+ * 영화 비교 ajax 통신 시작
+ */
+export const LODING_COMPARE_MOVIE_LIST = 'LODING_COMPARE_MOVIE_LIST'
+
+/**
+ * 영화 비교 ajax 통신 종료
+ */
+export const END_COMPARE_MOVIE_LIST = 'END_COMPARE_MOVIE_LIST'
+
+/**
+ * 영화 비교 성공
+ */
+export const SUCCESS_COMPARE_MOVIE_LIST = 'SUCCESS_COMPARE_MOVIE_LIST'
+
+/**
+ * 영화 비교 실패
+ */
+export const FAIL_COMPARE_MOVIE_LIST = 'FAIL_COMPARE_MOVIE_LIST'
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// 검색
+/**
+ * 검색 시작
+ */
+export const GET_SEARCH = 'GET_SEARCH'
+
+/**
+ * 검색 ajax 통신 시작
+ */
+export const LODING_SEARCH = 'LODING_SEARCH'
+
+/**
+ * 검색 ajax 통신 종료 
+ */
+export const END_SEARCH = 'END_SEARCH'
+
+/**
+ * 검색 성공
+ */
+export const SUCCESS_SEARCH = 'SUCCESS_SEARCH'
+
+/**
+ * 검색 실패
+ */
+export const FAIL_SEARCH = 'FAIL_SEARCH'
